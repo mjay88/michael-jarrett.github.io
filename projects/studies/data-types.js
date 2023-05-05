2a5145e038e07417c6e10ab469b87101cf62dbac
Michael W Jarrett <mwilliamkjarrett@gmail.com>
2023-05-05 01:24:27 +0000

    started data types

---
+++ projects/studies/data-types.js
@@ -0,0 +1,57 @@
+/**
+ * 
+ * Data Types
+ * 
+ * 
+ * 
+ * Clear explanation for each concept
+ * 
+ *
+ * Working code exampls for each concept
+ * 
+ * 
+ *Commmet of what each code example should log/return 
+ * 
+ *
+ * 
+ * 
+ */
+
+
+ //1. Number
+
+
+ //2. String
+
+
+ //3. Boolean
+
+
+ //4. Array
+
+
+ //5. Object
+
+
+ //6. Function
+
+
+
+ //7. undefined
+
+
+
+ //8. null
+
+
+
+//9. NaN
+
+
+//10. Infinity and -Infinity
+
+
+//11.Primitive vs Simple data types
+
+
+//12. Copy by value vs copy by reference.
